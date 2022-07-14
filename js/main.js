b156976f413fb4c43bace7502ffc0723535ad04d
Valerio <canivalerio93@gmail.com>
2022-07-14 13:04:09 +0200

    aggiunto bonus difficoltà dell'esercizio precedente come nuovo punto di partenza

--- js/main.js
+++ js/main.js
@@ -1,29 +1,45 @@
 let start = document.getElementById('play');
-start.addEventListener('click', 
-    function(){
+start.addEventListener('click', startgrid);
 
-        const griglia = document.getElementById('griglia');
-        griglia.innerHTML="";
+function startgrid(){
 
-        for (let i = 0; i < 100; i++) {
+    const difficulty = parseInt(document.getElementById('difficulty').value);
+    console.log(difficulty);
 
-            const square = createSquare(i + 1);
-            square.addEventListener('click', 
-                function () {
-                    this.classList.toggle('clicked');
-                    console.log(i + 1);
-                }
-            );
+    let cellBase = '';
 
-            griglia.append(square);
-        };
-    }
-);
+    if(difficulty == 1){
+        cellBase = 10;
+    } else if (difficulty == 2){
+        cellBase = 9;
+    } else if (difficulty == 3){
+        cellBase = 7;
+    };
 
-function createSquare(numero) {
+    const griglia = document.getElementById('griglia');
+    griglia.innerHTML="";
+
+    for (let i = 0; i < (cellBase * cellBase); i++) {
+
+        const square = createSquare(i + 1 , cellBase);
+        square.addEventListener('click', 
+            function () {
+                this.classList.toggle('clicked');
+                console.log(i + 1);
+            }
+        );
+
+        griglia.append(square);
+    };
+}
+
+
+function createSquare(numero, cellSide) {
 
     const newCell = document.createElement('div');
     newCell.classList.add('cell', 'cellcolor');
+    newCell.style.height = 'calc(100% / ' + cellSide + ')';
+    newCell.style.width = 'calc(100% / ' + cellSide + ')';
     
     const numberSquare = document.createElement('div');
     numberSquare.append(numero);
